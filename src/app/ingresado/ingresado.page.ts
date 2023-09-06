74fe8a837b51a1f3394d126ce389bb33c6d12668
chinoowo <di.croma@duocuc.cl>
2023-09-06 11:21:53 -0300

    Detalles en mostrar usuario en page ingresado

--- src/app/ingresado/ingresado.page.ts
+++ src/app/ingresado/ingresado.page.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
-import { AlertController ,NavController} from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ingresado',
@@ -15,15 +9,23 @@ import { AlertController ,NavController} from '@ionic/angular';
 export class IngresadoPage implements OnInit {
   nombre: string = ''; // Inicializa la variable con un valor por defecto
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
-  ngOnInit() {
-    // Intenta obtener el nombre de usuario almacenado en localStorage
+  ionViewWillEnter() {
+    // En el evento ionViewWillEnter, obtén el nombre de usuario actualizado del localStorage
     const usuarioJSON = localStorage.getItem('usuario');
 
     if (usuarioJSON) {
       const usuario = JSON.parse(usuarioJSON);
       this.nombre = usuario.nombre; // Asigna el nombre a la variable
     }
+  }
+
+  volverARegistro() {
+    this.navCtrl.navigateBack(['/registro']);
+  }
+
+  ngOnInit() {
+    // Mantén el código existente aquí si hay alguna otra lógica de inicialización
   }
 }

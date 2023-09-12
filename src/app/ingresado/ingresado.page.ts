f8cada70328a26d3688cffe67c87e306d864f427
chinoowo <di.croma@duocuc.cl>
2023-09-12 10:56:47 -0300

    scaner listto?

--- src/app/ingresado/ingresado.page.ts
+++ src/app/ingresado/ingresado.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { BarcodeScanner } from 'capacitor-barcode-scanner';
 import { AlertController } from '@ionic/angular';
 
 
@@ -15,9 +15,18 @@ export class IngresadoPage implements OnInit {
   rut: string = '';
   carrera: string = '';
   isSupported = false;
-  barcodes: Barcode[] = [];
+
 
   constructor(private navCtrl: NavController,private alertController: AlertController) { }
+
+
+async scan(){
+  const resultadoQr = (await BarcodeScanner.scan()).code;
+  alert("Codigo --->" + resultadoQr)
+  
+
+}
+
 
   ionViewWillEnter() {
     // En el evento ionViewWillEnter, obtén el nombre de usuario actualizado del localStorage
@@ -46,30 +55,8 @@ export class IngresadoPage implements OnInit {
   }
 
   ngOnInit() {
-    BarcodeScanner.isSupported().then((result) => {
-      this.isSupported = result.supported;
-    });
+
   }
-  async scan(): Promise<void> {
-    const granted = await this.requestPermissions();
-    if (!granted) {
-      this.presentAlert();
-      return;
-    }
-    const { barcodes } = await BarcodeScanner.scan();
-    this.barcodes.push(...barcodes);
-  }
-  async requestPermissions(): Promise<boolean> {
-    const { camera } = await BarcodeScanner.requestPermissions();
-    return camera === 'granted' || camera === 'limited';
-  }
-  async presentAlert(): Promise<void> {
-    const alert = await this.alertController.create({
-      header: 'Permiso denegado',
-      message: 'Por favor, conceda permiso a la cámara para utilizar el escáner de código de barras',
-      buttons: ['OK'],
-    });
-    await alert.present();
-  }
+  
 
 }

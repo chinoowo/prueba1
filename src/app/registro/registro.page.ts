2d38d3036dd42bb526a9775bee732a504a732dde
chinoowo <di.croma@duocuc.cl>
2023-10-24 14:15:27 -0300

    comsumo api regiones y comunas, falta algo

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -6,6 +6,9 @@ import {
   FormBuilder
 } from '@angular/forms';
 import { AlertController ,NavController} from '@ionic/angular';
+import { LocationService } from 'src/app/services/location.service';
+import { Comuna } from '../models/comuna';
+import { Region } from '../models/region';
 
 @Component({
   selector: 'app-registro',
@@ -16,22 +19,45 @@ import { AlertController ,NavController} from '@ionic/angular';
 
 export class RegistroPage implements OnInit {
 
+
+
+  regiones:Region[]=[];
+  comunas:Comuna[]=[];
+  regionSel:number = 0;
+  comunaSel:number = 0;
+  seleccionComuna:boolean = true;
+
   formularioRegistro: FormGroup;
   
   constructor(public fb: FormBuilder,
     public alertController: AlertController,
-    public navCtrl: NavController) {
+    public navCtrl: NavController,
+    private locationService:LocationService) {
     this.formularioRegistro = this.fb.group({
       'nombre': new FormControl("", Validators.required),
       'carrera': new FormControl("",Validators.required),
       'rut': new FormControl("", Validators.required),
       'password': new FormControl("", Validators.required),
-      'confirmacionPassword': new FormControl("", Validators.required)
+      'confirmacionPassword': new FormControl("", Validators.required),
     });
   }
 
   ngOnInit() {
+    this.cargarRegion();
   }
+
+  async cargarRegion(){
+    const req = await this.locationService.getRegion();
+    this.regiones = req.data;
+  }
+
+  async cargarComuna(){
+    this.seleccionComuna = false;
+    const req = await this.locationService.getComuna(this.regionSel);
+    this.comunas = req.data;
+  }
+
+
 
   async guardar(){
     var f = this.formularioRegistro.value;
---
+++ src/app/services/location.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+import { ApiResponse } from '../models/apiResponse';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  constructor(private http: HttpClient) { }
+
+
+  async getRegion() {
+    return await lastValueFrom(this.http.get<ApiResponse<any>>(`${environment.apiUrl}region`))
+  }
+
+  async getComuna(idRegion: number) {
+    return await lastValueFrom(this.http.get<ApiResponse<any>>(`${environment.apiUrl}comuna/` + idRegion))
+  }
+}
